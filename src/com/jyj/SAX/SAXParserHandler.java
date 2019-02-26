794c86f0f409e78cb7b6621ea1c4b3c8dc569359
lipeijing <lipeijing@jd.com>
2019-02-26 14:34:10 +0800

    SAX解析xml文件每一项内容，记录持久化对象bookList

--- src/com/jyj/SAX/SAXParserHandler.java
+++ src/com/jyj/SAX/SAXParserHandler.java
@@ -4,6 +4,8 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.ArrayList;
+
 /**
  * @Classname SAXParserHandler
  * @Description TODO
@@ -13,6 +15,18 @@ import org.xml.sax.helpers.DefaultHandler;
 public class SAXParserHandler extends DefaultHandler {
 
     int bookIndex = 0;
+    String value = null;
+    Book book = null;
+    private ArrayList<Book> bookList = new ArrayList<Book>();
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
     /**
      * 用来遍历xml文件开始标签
      * @param uri
@@ -29,14 +43,18 @@ public class SAXParserHandler extends DefaultHandler {
 //            String value = attributes.getValue("id");
 //            System.out.println("book的属性值：" + value);
             bookIndex += 1;
+            book = new Book();
             System.out.println("=====开始遍历第" + bookIndex + "书=====");
             for (int i = 0; i < attributes.getLength(); i++) {
                 String name = attributes.getQName(i);
                 String name_value = attributes.getValue(i);
-                System.out.println("第" + (i+1) + "个属性名" + name + "值为" + name_value);
+//                System.out.println("第" + (i+1) + "个属性名" + name + "值为" + name_value);
+                if (attributes.getQName(i).equals("id")) {
+                    book.setId(Integer.parseInt(name_value));
+                }
             }
         } else if (!qName.equals("book") && !qName.equals("bookstore")) {
-            System.out.print("节点名：" + qName + "值为：");
+//            System.out.print("节点名：" + qName + "值为：");
         }
     }
 
@@ -51,7 +69,19 @@ public class SAXParserHandler extends DefaultHandler {
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
         if (qName.equals("book")) {
+            bookList.add(book);
+            book = null;
             System.out.println("=====结束遍历第" + bookIndex + "书=====");
+        } else if (qName.equals("name")) {
+            book.setName(value);
+        } else if (qName.equals("author")) {
+            book.setAuthor(value);
+        } else if (qName.equals("year")) {
+            book.setYear(value);
+        } else if (qName.equals("price")) {
+            book.setPrice(value);
+        } else if (qName.equals("language")) {
+            book.setLanguage(value);
         }
     }
 
@@ -78,9 +108,9 @@ public class SAXParserHandler extends DefaultHandler {
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         super.characters(ch, start, length);
-        String value = new String(ch, start, length);
+        value = new String(ch, start, length);
         if (!value.trim().equals("")) {
-            System.out.println(value);
+//            System.out.println(value);
         }
     }
 }
--- src/com/jyj/SAX/SAXTest.java
+++ src/com/jyj/SAX/SAXTest.java
@@ -22,5 +22,16 @@ public class SAXTest {
         //
         SAXParserHandler handler = new SAXParserHandler();
         parser.parse("books.xml", handler);
+        System.out.println("共有：" + handler.getBookList().size() + "本书");
+        for (Book book :
+                handler.getBookList()) {
+            System.out.println(book.getId());
+            System.out.println(book.getName());
+            System.out.println(book.getAuthor());
+            System.out.println(book.getYear());
+            System.out.println(book.getPrice());
+            System.out.println(book.getLanguage());
+            System.out.println("=========");
+        }
     }
 }
